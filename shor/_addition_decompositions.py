1f067063ebe972b85f69a0b19f1da268bb892500
Craig Gidney <craiggidney@google.com>
2017-04-13 20:58:14 -0700

    add larger

--- shor/_addition_decompositions.py
+++ shor/_addition_decompositions.py
@@ -4,11 +4,12 @@ from __future__ import unicode_literals
 from projectq.cengines import DecompositionRule
 from projectq.ops import X, Swap, All, C, C_star
 from ._addition_gates import AdditionGate
+from ._increment_gates import Increment, Decrement
 
 
-def do_addition_with_same_size_and_no_controls(eng, input_reg, target_reg):
+def do_addition_with_same_size_and_no_controls(input_reg, target_reg):
     """
-    Reversibly subtracts one register out of another of the same size.
+    Reversibly adds one register into another of the same size.
 
     N: len(input_reg) + len(target_ref)
     Size: O(N)
@@ -85,25 +86,103 @@ def do_addition_with_same_size_and_no_controls(eng, input_reg, target_reg):
     cnots | (carry_bit, input_reg[:-1] + target_reg)
 
 
-def do_addition_no_controls(eng, input_reg, target_reg):
+def do_addition_with_larger_target_and_no_controls(input_reg, target_reg):
+    """
+    Reversibly adds one register into another larger one size.
+
+    N: len(input_reg) + len(target_ref)
+    Size: O(N)
+    Depth: O(N)
+
+    Sources:
+        Takahashi and Kunihiro, 2005
+        "A linear-size quantum circuit for addition with no ancillary qubits"
+
+        Yvan Van Rentergem and Alexis De Vos, 2004
+        "Optimal Design of A Reversible Full Adder"
+
+    Diagram:
+                     ╭──●───╮
+       ┌───┐         │ ┌┴─┐ │
+       ┤   ├       ──┼─┤  ├─┼────────×─────────────────────────────────────×─●─
+       │   │         │ │  │ │        │                                     │ │
+       ┤   ├       ──┼─┤  ├─┼────────┼────×───────────────────────────×─●──┼─┼─
+       │   │         │ │  │ │        │    │                           │ │  │ │
+       ┤inp├       ──┼─┤  ├─┼────────┼────┼────×─────────────────×─●──┼─┼──┼─┼─
+       │ A │         │ │  │ │        │    │    │                 │ │  │ │  │ │
+       ┤   ├       ──┼─┤  ├─┼────────┼────┼────┼────×───────×─●──┼─┼──┼─┼──┼─┼─
+       │   │         │ │  │ │        │    │    │    │       │ │  │ │  │ │  │ │
+       ┤   ├       ──╯ │  │ ╰─●────●─×──●─×──●─×──●─×───●───×─┼──×─┼──×─┼──×─┼─
+       └─┬─┘           │−1│   │    │ │  │ │  │ │  │ │   │   │ │  │ │  │ │  │ │
+       ┌─┴─┐   =       │  │  ┌┴─┐  │ │  │ │  │ │  │ │   │   │ │  │ │  │ │  │ │
+       ┤   ├       ────┤  ├──┤  ├──⊕─●──┼─●──┼─┼──┼─┼───┼───┼─┼──┼─┼──┼─┼──●─⊕─
+       │   │           │  │  │  │       │ │  │ │  │ │   │   │ │  │ │  │ │
+       ┤   ├       ────┤  ├──┤  ├───────⊕─●──┼─┼──┼─┼───┼───┼─┼──┼─┼──●─⊕──────
+       │   │           │  │  │  │            │ │  │ │   │   │ │  │ │
+       ┤+=A├       ────┤  ├──┤+1├────────────⊕─●──┼─┼───┼───┼─┼──●─⊕───────────
+       │   │           │  │  │  │                 │ │   │   │ │
+       ┤   ├       ────┤  ├──┤  ├─────────────────⊕─●───┼───●─⊕────────────────
+       │   │        e  │  │  │  │                      ┌┴─┐
+       ┤   ├       ━/━━┥  ┝━━┥  ┝━━━━━━━━━━━━━━━━━━━━━━┥+1┝━━━━━━━━━━━━━━━━━━━━
+       └───┘           └──┘  └──┘                      └──┘
+                       (1)    (2)  -------(3)--------  (4)  --------(5)--------
+    Args:
+        eng (projectq.cengines.BasicEngine): Engine.
+        input_reg (projectq.types.Qureg):
+            The source register. Used as workspace, but ultimately not affected
+            by the operation.
+            end.
+        target_reg (projectq.types.Qureg):
+            The destination register, whose value is increased by the value in
+            the source register.
+    """
+    assert len(input_reg) <= len(target_reg)
+    n = len(input_reg)
+    if n == 0:
+        return
+
+    carry_bit = input_reg[-1]
+    cnot = C(X)
+    cswap = C(Swap)
+
+    # (1) Dirty MSB correction.
+    C(Decrement) | (carry_bit, input_reg[:-1] + target_reg)
+
+    # (2) Handle MSB separately.
+    C(Increment) | (carry_bit, target_reg)
+
+    # (3) Ripple forward.
+    for i in range(n - 1):
+        cnot | (carry_bit, target_reg[i])
+        cswap | (target_reg[i], carry_bit, input_reg[i])
+
+    # (4) Carry into high output bits.
+    cnot | (carry_bit, target_reg[n-1:])
+
+    # (5) Ripple backward.
+    for i in range(n - 1)[::-1]:
+        cswap | (target_reg[i], carry_bit, input_reg[i])
+        cnot | (input_reg[i], target_reg[i])
+
+
+def do_addition_no_controls(input_reg, target_reg):
     # When input is larger, just ignore its high bits.
     if len(input_reg) >= len(target_reg):
         do_addition_with_same_size_and_no_controls(
-            eng, input_reg[:len(target_reg)], target_reg)
+            input_reg[:len(target_reg)], target_reg)
         return
 
-    # When target is larger, need to get fancier.
-    raise NotImplementedError()
+    do_addition_with_larger_target_and_no_controls(input_reg, target_reg)
 
 
-def do_addition(eng, input_reg, target_reg, dirty, controls):
+def do_addition(input_reg, target_reg, dirty, controls):
     if len(controls) == 0:
-        return do_addition_no_controls(eng, input_reg, target_reg)
+        return do_addition_no_controls(input_reg, target_reg)
 
     # Remove controls with double-add-invert trick.
     expanded = [dirty] + target_reg
     for _ in range(2):
-        do_addition_no_controls(eng, input_reg, expanded)
+        do_addition_no_controls(input_reg, expanded)
         C_star(All(X)) | (controls, expanded)
         All(X) | expanded
 
@@ -112,7 +191,6 @@ all_defined_decomposition_rules = [
     DecompositionRule(
         gate_class=AdditionGate,
         gate_decomposer=lambda cmd: do_addition(
-            cmd.engine,
             input_reg=cmd.qubits[0],
             target_reg=cmd.qubits[1],
             controls=cmd.control_qubits,
@@ -122,7 +200,6 @@ all_defined_decomposition_rules = [
     DecompositionRule(
         gate_class=AdditionGate,
         gate_decomposer=lambda cmd: do_addition_no_controls(
-            cmd.engine,
             input_reg=cmd.qubits[0],
             target_reg=cmd.qubits[1]),
         max_controls=0),
--- shor/_addition_test.py
+++ shor/_addition_test.py
@@ -5,10 +5,17 @@ from __future__ import unicode_literals
 import random
 
 from projectq import MainEngine
-from projectq.cengines import DummyEngine, AutoReplacer, DecompositionRuleSet
+from projectq.cengines import (DummyEngine,
+                               AutoReplacer,
+                               DecompositionRuleSet,
+                               LimitedCapabilityEngine)
 from projectq.ops import Allocate, Swap, X, C
 from projectq.setups.decompositions import swap2cnot
-from . import _multi_not_decompositions, _addition_decompositions
+from . import (
+    _multi_not_decompositions,
+    _addition_decompositions,
+    _increment_decompositions
+)
 from ._addition_decompositions import (
     do_addition_with_same_size_and_no_controls
 )
@@ -21,7 +28,7 @@ def test_exact_commands_for_small_circuit():
     eng = MainEngine(backend=backend, engine_list=[])
     src = eng.allocate_qureg(2)
     dst = eng.allocate_qureg(2)
-    do_addition_with_same_size_and_no_controls(eng, src, dst)
+    do_addition_with_same_size_and_no_controls(src, dst)
 
     a, c = src[0], src[1]
     x, y = dst[0], dst[1]
@@ -44,15 +51,35 @@ def test_exact_commands_for_small_circuit():
     ] for cmd in cmds]
 
 
-def test_fuzz_add():
+def test_fuzz_add_same_size():
     for _ in range(10):
         n = random.randint(1, 100)
         fuzz_permutation_against_circuit(
             register_sizes=[n, n],
             outputs_for_input=lambda a, b: (a, b + a),
-            engine_list=[AutoReplacer(DecompositionRuleSet(modules=[
-                swap2cnot,
-                _multi_not_decompositions,
-                _addition_decompositions
-            ]))],
+            engine_list=[
+                AutoReplacer(DecompositionRuleSet(modules=[
+                    swap2cnot,
+                    _multi_not_decompositions,
+                    _addition_decompositions
+                ])),
+                LimitedCapabilityEngine(allow_toffoli=True)],
+            actions=lambda eng, regs: Add | (regs[0], regs[1]))
+
+
+def test_fuzz_add_into_large():
+    for _ in range(10):
+        n = random.randint(1, 100)
+        e = random.randint(1, 100)
+        fuzz_permutation_against_circuit(
+            register_sizes=[n, n + e, 1],
+            outputs_for_input=lambda a, b, d: (a, b + a, d),
+            engine_list=[
+                AutoReplacer(DecompositionRuleSet(modules=[
+                    swap2cnot,
+                    _multi_not_decompositions,
+                    _addition_decompositions,
+                    _increment_decompositions
+                ])),
+                LimitedCapabilityEngine(allow_toffoli=True)],
             actions=lambda eng, regs: Add | (regs[0], regs[1]))
